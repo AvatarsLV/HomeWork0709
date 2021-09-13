599ca82640e8c2f4376aee9459e84b40d778c124
Atvars Trams <atvars.trams@fizmati.lv>
2021-09-13 17:46:45 +0300

    Homework start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kā Tevi sauc?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Sveiks, {userName}");
+            Console.WriteLine("=============================================================");
+
+            Console.WriteLine("Kāds ir Tavs vecums?");
+            string userAge = Console.ReadLine();
+
+            int age = int.Parse(userAge);
+            int ageNextYear = age + 1;
+            bool Is18 = age >= 18;
+
+
+            Console.WriteLine($"Nākamgad Tev paliks {ageNextYear}, Tu esi pilngadīgs:  {Is18}");
+
+        }
+    }
+}
